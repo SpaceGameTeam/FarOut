ee713475fe2757cfe6adb39a4a4d90fcf8f2d92b
mackfoggia <mack.foggia@yahoo.com>
2021-07-07 16:58:35 -0700

    Added basic structure for GameObject class, including files and abstract base class.

---
+++ GameObject.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "SystemClass.h"
+
+
+class GameObject {
+public:
+	GameObject();
+
+	virtual void update(sf::Time dt) = 0;
+	virtual void draw(sf::RenderWindow window) = 0;
+
+private:
+
+};
