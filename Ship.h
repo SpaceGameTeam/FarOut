508f26d6b003d0668ff8b895bb02b6a67aecbb10
Amila Ferron <aferron@pdx.edu>
2021-07-08 15:51:10 -0700

    Add draw function

--- Ship.h
+++ Ship.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+// Change this #include to FarOut.h eventually
 #include "GameObject.h"
 
 
 class Ship : public GameObject {
 public:
 	Ship();
+    void draw(sf::RenderTarget& target, sf::RenderStates states)const;
 
 private:
     sf::ConvexShape Body;
