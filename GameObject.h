503f151fabcd8ebe492f07f6ed6904481b9f0c36
TaylorKNoah <73961491+TaylorKNoah@users.noreply.github.com>
2021-07-07 21:42:43 -0700

    toying with VisableObject class idea

--- GameObject.h
+++ GameObject.h
@@ -4,10 +4,24 @@
 #include "SystemClass.h"
 
 
+
 class GameObject {
 public:
 	GameObject();
 
 	virtual void update(sf::Time dt) = 0;
 
+};
+
+
+class VisableObject: public GameObject {
+public:
+	VisableObject();
+
+	virtual void update(sf::Time dt) = 0;
+	virtual void draw(sf::RenderWindow window) = 0;
+
+protected:
+	sf::Texture texture;
+	sf::Sprite sprite;
 };
