1ea63c50c64be8d34082c7dc2d44799bdb0cb4dd
mackfoggia <mack.foggia@yahoo.com>
2021-07-15 20:47:01 -0700

    Changed the background to tile based on a set factor, it is quite large now

--- PrototypeScene.cpp
+++ PrototypeScene.cpp
@@ -6,8 +6,16 @@
 //Background stuff
 
 Background::Background() {
+
+	// These should be adjusted to use system wide constants when those are implemented
+	int x = 1920;
+	int y = 1080;
+	int universeSize = 1000;
 	texture.loadFromFile("Assets/pixelated_halo_starfield_by_necro_skeletal_d56he85.png");
-	sprite.setTexture(texture);
+	texture.setRepeated(true);
+	sprite.setTextureRect(sf::IntRect(0, 0, universeSize * x, universeSize * y));
+	sprite.setTexture(texture, false);
+	setPosition(-(universeSize * x / 2), -(universeSize * y / 2));
 }
 
 void Background::update(sf::Time dt) {}
