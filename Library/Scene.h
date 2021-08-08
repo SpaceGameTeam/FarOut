fc34247bf639719f337085beb689858305b75327
Jeffrey Jernstrom <jeffrey.jernstrom@google.oom>
2021-08-07 20:08:32 -0700

    Forgot to include this in the last commit.

---
+++ Library/Scene.h
@@ -0,0 +1,22 @@
+// Scene base class for client derivation
+
+
+
+#ifndef SCENE
+#define SCENE
+#include <SFML/Graphics.hpp>
+
+//#include "SystemClass.h"
+
+
+class Scene {
+public:
+	Scene();
+
+	virtual void update(sf::Time dt) = 0;
+	virtual void draw(sf::RenderWindow &window) = 0;
+
+private:
+	// Some sort of collection of objects? Maybe these should be in the derived class?
+};
+#endif
