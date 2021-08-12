5591dd503d8a0243e31cc22344bb61e640cea617
mackfoggia <mack.foggia@yahoo.com>
2021-08-12 16:32:01 -0700

    Cleanup to various system files

--- FarOut/GameObject.h
+++ FarOut/GameObject.h
@@ -1,9 +1,8 @@
+// FarOut - GameObject Class
 
 #ifndef GAMEOBJ
 #define GAMEOBJ
 #include <SFML/Graphics.hpp>
-
-
 
 class GameObject : public sf::Drawable, public sf::Transformable {
 public:
@@ -11,6 +10,6 @@ public:
 
 	virtual void update(sf::Time dt) = 0;
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const = 0;
-
 };
+
 #endif
